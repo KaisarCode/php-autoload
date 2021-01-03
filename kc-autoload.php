e05303de00b3306e733774e85da5724248db6207
KaisarCode <kaisar@kaisarcode.com>
2021-01-03 02:35:54 -0300

    kc classes autoloader

---
+++ kc-autoload.php
@@ -0,0 +1,15 @@
+<?php
+// KaisarCode classes autoloader
+function kcAutoload($dir = '') {
+    spl_autoload_register(
+    function ($cls) use ($dir) {
+        $ds = DIRECTORY_SEPARATOR;
+        $cn = explode('\\', $cls);
+        $nm = end($cn);
+        $fl = strtolower($nm);
+        !empty($dir)?$dir.=$ds:$dir='';
+        $dir .= "php-{$fl}{$ds}";
+        $dir .= "{$fl}.class.php";
+        include_once ($dir);
+    });
+}
